c39b08aa04a9b276e1c1ac41041f286ee8cdb417
TrustlyApps <Bindomarcos13@gmail.com>
2023-03-22 00:32:53 -0300

    Fixed: - read file - validations over lines
    Added:
    - write file method
    - backup method
    - logger for processed file
    - file extension validation

--- camelToSnake.py
+++ camelToSnake.py
@@ -1,7 +1,9 @@
 import os
 
+
 def is_camel_case(word):
     return word != word.lower() and word != word.upper() and "_" not in word
+
 
 def to_snake_case(word):
     fragments = [word[0].lower()]
@@ -13,21 +15,51 @@ def to_snake_case(word):
             fragments.append(c)
     return "".join(fragments)
 
-def iterateFiles():
+
+def write_file(file, content):
+    with open(file, "w") as f_out:
+        f_out.write(content)
+
+
+def build_line(words):
+    return ' '.join(words) + '\n'
+
+
+def iterate_files():
     directory = input("Type the root directory: ")
+    extension = input("Type the file extension (e.g. '.txt'): ")
+    backup_files = input("Create backup files? (y/n): ").lower() == "y"
 
     for root, dirs, files in os.walk(directory):
         for file in files:
-            file_path = os.path.join(root, file)
-            with open(file_path, "r+") as f_in:
-                for line in f_in:
-                    words = line.split()
-                    for i, word in enumerate(words):
-                        if is_camel_case(word):
-                            words[i] = to_snake_case(word)
-                        f_in.write(words + "\n")
+            if file.endswith(extension):
+                file_path = os.path.join(root, file)
+                try:
+                    with open(file_path, "r") as f_in:
+                        replaced_content = ""
+                        for line in f_in:
+                            words = line.split()
+                            if line == "\n":
+                                replaced_content += "\n"
+                            else:
+                                for i, word in enumerate(words):
+                                    if is_camel_case(word):
+                                        words[i] = to_snake_case(word)
+                                new_line = build_line(words)
+                                replaced_content += new_line
+
+                        if backup_files:
+                            backup_path = file_path + ".bak"
+                            write_file(backup_path, f_in.read())
+
+                        write_file(file_path, replaced_content)
+
+                    print(f"{file_path} processed successfully.")
+                except Exception as e:
+                    print(f"Error processing {file_path}: {str(e)}")
+                    continue
 
     print("Files changed")
 
 
-iterateFiles()
+iterate_files()
